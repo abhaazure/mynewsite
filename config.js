fafdeab1bb798cce13156502e2db5f6f36645e8a
Iain Foulds <iain@azuremol.com>
2021-08-24 21:06:07 +0000

    Pizza

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+config.endpoint = 'https://testdb-cosmos.documents.azure.com:443/'
+config.key = 'JEGuXituBo7fgdY2iQb5mc3Hg0JUIqHi1qrfZNzKMCu7xib3kkUrzSJrohkIIqRK466HmbgTq5hUMtPr0aWueg=='
+
+config.database = { id: 'pizzadb' }
+config.container = { id: 'pizzas' }
+
+module.exports = config;
